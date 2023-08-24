647a0d7dd86abbff60fadc3e377265dd36210c8a
JuaNFRaN2031 <juanfran2000juanfran2000@gmail.com>
2023-08-24 14:40:32 +0200

    .

--- adidas/index.php
+++ adidas/index.php
@@ -1,10 +1,155 @@
 <!DOCTYPE html>
 <html lang="es-ES">
 <head>
+    <!--
+    https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_image_overlay_opacity
+    -->
     <meta charset="UTF-8">
-    <title>Title</title>
-</head>
+    <title>Adidas</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="img/icono.jpg" type="image/x-icon">
+    <link href="https://fonts.cdnfonts.com/css/buttershine-serif" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:light' rel='stylesheet' type='text/css'>
+    <script src="js/script.js"></script>
 <body>
+
+    <header>
+        <!-- Línea horizontal con fondo negro -->
+        <div class="black-hr">
+
+        </div>
+
+        <div class="topnav">
+            <a href="index.php"><img src="img/Adidas-logo.png" style="width: 65px"></a>
+            <a class="topnav-centrado" href="#clientes">HOMBRE</a>
+            <a class="topnav-centrado" href="#clientes">MUJER</a>
+            <a class="topnav-centrado" href="#clientes">NIÑXS</a>
+            <a class="topnav-centrado" href="#productos">NOVEDADES</a>
+            <a class="topnav-centrado" href="#clientes">RESEÑAS</a>
+            <input class="topnav-centrado" type="text" placeholder="Buscar..">
+        </div>
+        <div class="videoBanner">
+            <video src="video/yt1s.com%20-%20adidas%20Basketball%20%20DAME%20TIME%20The%20Damian%20Lillard%20Story%20%20Chapter%20One%20Dame%20Time.mp4" autoplay muted loop></video>
+        </div>
+    </header>
+<br>
+<br>
+    <section id="productos">
+        <h1 class="titulos">NUESTROS PRODUCTOS</h1>
+        <div class="producto-foto">
+            <div class="container">
+                <img class="image" src="img/Zapatilla_Adizero_Boston_9.jpg" alt="AdizeroBoston">
+                <div class="middle">
+                    <h2>Adizero Boston 9</h2>
+                    <br>
+                    <h2>140€</h2>
+                </div>
+            </div>
+            <div class="container">
+                <img class="image" src="img/Zapatilla_Basket_Profi.jpg" alt="BasketProfi">
+                <div class="middle">
+                    <h2>Basket Profi</h2>
+                    <br>
+                    <h2>70€</h2>
+                </div>
+            </div>
+            <div class="container">
+                <img class="image" src="img/Zapatilla_Broomfield_Verde.jpg" alt="Broomfield">
+                <div class="middle">
+                    <h2>Broomfield</h2>
+                    <br>
+                    <h2>115€</h2>
+                </div>
+            </div>
+        </div>
+        <div class="producto-foto">
+            <div class="container">
+                <img class="image" src="img/Zapatilla_Continental_80.jpg" alt="Continental80">
+                <div class="middle">
+                    <h2>Continental 80</h2>
+                    <br>
+                    <h2>110€</h2>
+                </div>
+            </div>
+            <div class="container">
+                <img class="image" src="img/Zapatilla_NMD_R1_Burgundy_.jpg" alt="NMD R1Burgundy">
+                <div class="middle">
+                    <h2>NMD_R1_Burgundy</h2>
+                    <br>
+                    <h2>150€</h2>
+                </div>
+            </div>
+            <div class="container">
+                <img class="image" src="img/Zapatilla_Stan_Smith.jpg" alt="StanSmith">
+                <div class="middle">
+                    <h2>Stan Smith Disney Wall-E</h2>
+                    <br>
+                    <h2>250€</h2>
+                </div>
+            </div>
+        </div>
+        <div class="producto-foto">
+            <div class="container">
+                <img class="image" src="img/Zapatilla_Ultraboost_Clima_DNA_Naranja.jpg" alt="UltraboostClimaDNA">
+                <div class="middle">
+                    <h2>Ultraboost Clima DNA Naranja</h2>
+                    <br>
+                    <h2>148€</h2>
+                </div>
+            </div>
+            <div class="container">
+                <img class="image" src="img/Zapatilla_Superstar.jpg" alt="Superstar">
+                <div class="middle">
+                    <h2>Superstar</h2>
+                    <br>
+                    <h2>120€</h2>
+                </div>
+            </div>
+            <div class="container">
+                <img class="image" src="img/Zapatilla_ZX_2K_Boost_Blanco.jpg" alt="ZX 2K Boost">
+                <div class="middle">
+                    <h2>ZX 2K Boost</h2>
+                    <br>
+                    <h2>150€</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="clientes">
+        <h1 class="titulos">RESEÑAS DE NUESTROS CLIENTES</h1>
+        <div class="cliente1">
+            <p>
+                Mi viaje como cliente de Adidas ha sido una verdadera experiencia de satisfacción y estilo. La página web de la marca y su gama de productos han establecido un estándar impresionante que combina a la perfección la comodidad moderna con el estilo distintivo.
+            </p>
+            <img src="img/original.png" class="img-clientes">
+        </div>
+        <div class="cliente2">
+            <img src="img/bienestar-optimismo-f-archivo.png" class="img-clientes">
+            <p>
+                Increíblemente satisfecha con mi experiencia en la página web de Adidas y sus emocionantes productos!
+                Como un apasionado del deporte y la moda, he encontrado en esta marca una combinación perfecta de estilo, calidad y rendimiento.
+            </p>
+        </div>
+        <div class="cliente1">
+            <p>
+                Como cliente de Adidas, he encontrado la página web y los productos de la marca excepcionales en términos de calidad, estilo y variedad. La combinación de tecnología innovadora y diseño elegante hace que Adidas siga siendo una opción líder para aquellos que buscan productos deportivos y de moda de alta calidad. Mi experiencia general ha sido positiva y no dudo en recomendar tanto la página web como los productos de Adidas a otros entusiastas del deporte y la moda.
+            </p>
+            <img src="img/person-smiling.png" class="img-clientes">
+        </div>
+    </section>
+    <footer>
+        <div class="footer-container">
+            <div class="footer-links">
+                <a href="#">CONTACTO</a>
+                <a href="#">PRODUCTOS</a>
+            </div>
+            <div class="footer-social">
+                <a target="_blank" href="https://www.instagram.com/adidas/"><img src="img/social-insta.png" alt="Instagram"></a>
+                <a target="_blank" href="https://www.facebook.com/adidas"><img src="img/social-face.png" alt="Facebook"></a>
+                <a target="_blank" href="https://twitter.com/adidas"><img src="img/social-tw.png" alt="Twitter"></a>
+            </div>
+        </div>
+    </footer>
 
 </body>
 </html>

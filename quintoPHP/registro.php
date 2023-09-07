9631a7efa1ad895a1b7511639bdba6b0a3abb322
JuaNFRaN2031 <juanfran2000juanfran2000@gmail.com>
2023-09-07 20:55:57 +0200

    .

--- quintoPHP/registro.php
+++ quintoPHP/registro.php
@@ -1,36 +1,40 @@
 <title>Gracias por contactar</title>
+<link rel="stylesheet" href="css/style.css">
 <?php
     include 'header.php';
     $nombre = $_GET['nombre'];
     $email = $_GET['email'];
-    $destinatario = $email;
-    $asunto = "Bienvenido a nuestra Web";
-    $cuerpo = '';
-    //para el envío en formato HTML
-    $headers = "MIME-Version: 1.0\r\n";
-    $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
-
-    //dirección del remitente
-    $headers .= "From: $email\r\n";
-
-    //dirección de respuesta, si queremos que sea distinta que la del remitente
-    $headers .= "Reply-To: irina@ceatformacion.com\r\n";
-
-    //ruta del mensaje desde origen a destino
-    $headers .= "Return-path: irina@ceatformación.com\r\n";
-
-    //direcciones que recibián copia
-    //	$headers .= "Cc: direccion@dominio.com\r\n";
-
-    //direcciones que recibirán copia oculta
-    //	$headers .= "Bcc: direccion@dominio.com,direccion@dominio.com\r\n";
-
-    mail($destinatario,$asunto,$cuerpo,$headers)
+//    $destinatario = $email;
+//    $asunto = "Bienvenido a nuestra Web";
+//    $cuerpo = '';
+//    //para el envío en formato HTML
+//    $headers = "MIME-Version: 1.0\r\n";
+//    $headers .= "Content-type: text/html; charset=iso-8859-1\r\n";
+//
+//    //dirección del remitente
+//    $headers .= "From: $email\r\n";
+//
+//    //dirección de respuesta, si queremos que sea distinta que la del remitente
+//    $headers .= "Reply-To: irina@ceatformacion.com\r\n";
+//
+//    //ruta del mensaje desde origen a destino
+//    $headers .= "Return-path: irina@ceatformación.com\r\n";
+//
+//    //direcciones que recibián copia
+//    //	$headers .= "Cc: direccion@dominio.com\r\n";
+//
+//    //direcciones que recibirán copia oculta
+//    //	$headers .= "Bcc: direccion@dominio.com,direccion@dominio.com\r\n";
+//
+//    mail($destinatario,$asunto,$cuerpo,$headers)
 ?>
 <h1>Registro de prueba</h1>
 <h3 style="margin-left: 10px">Gracias <?=ucfirst($nombre)?>, nos pondremos en contacto con usted</h3>
 <h3 style="margin-left: 10px">En breves recibirás un email a <a href="mailto:<?=$email?>" target="_blank" title="Ir al
  correo"><?=$email?></a></h3>
+<br>
+<a href="http://localhost:63342/CEAT-PHP/quintoPHP/contacto.php"
+   title="Volver a contacto.php" id="volver">Volver</a>
 <?php
     include 'footer.php';
 ?>

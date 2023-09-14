6e0f57bda76801b0fa89bd9f9e409171a9268141
JuaNFRaN2031 <juanfran2000juanfran2000@gmail.com>
2023-09-14 18:27:53 +0200

    .

---
+++ sextoPHP/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es-ES">
+<head>
+    <meta charset="UTF-8">
+    <title>Sexto PHP</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
+</head>
+<body>
+<!--<div class="row">-->
+    <div class="cuadro col-6">
+    </div>
+<!--</div>-->
+<script src="js/script.js"></script>
+</body>
+</html>

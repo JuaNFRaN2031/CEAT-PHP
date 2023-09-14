8344a8b0df1ffda97da94e0f5c584e482103ccb7
JuaNFRaN2031 <juanfran2000juanfran2000@gmail.com>
2023-09-14 18:27:55 +0200

    .

--- sextoPHP/index.php
+++ sextoPHP/index.php
@@ -1,8 +1,15 @@
+<?php
+    function mostrar($cad)
+    {
+        echo $cad . "<br>";
+    }
+?>
 <!DOCTYPE html>
 <html lang="es-ES">
 <head>
     <meta charset="UTF-8">
     <title>Sexto PHP</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nunito&display=swap" rel="stylesheet">
@@ -10,10 +17,47 @@
     <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
 </head>
 <body>
-<!--<div class="row">-->
-    <div class="cuadro col-6">
+<div class="row">
+    <div class="header-card-green col-6">
+        <h1>Manipular los números</h1>
+        <p>Las funciones más útiles para manipular los números son las siguientes:</p>
     </div>
-<!--</div>-->
+</div>
+<div class="row">
+    <div class="header-card-lightgreen col-6">
+        <h2>Valor absoluto de un número</h2>
+        <?php
+            $numero = -365;
+            mostrar("El valor absoluto de -365 es: " . abs($numero));
+            mostrar("El valor absoluto de 4587 es: " . abs(4587));
+        ?>
+    </div>
+</div>
+<div class="row">
+    <div class="header-card-vanilla col-6">
+        <h2>Redondeo de un número al entero superior</h2>
+    </div>
+</div>
+<div class="row">
+    <div class="header-card-dark-vanilla col-6">
+        <h2>Redondeo de un número al entero inferior</h2>
+    </div>
+</div>
+<div class="row">
+    <div class="header-card-brown col-6">
+        <h2>Cociente de la división entera de dos enteros</h2>
+    </div>
+</div>
+<div class="row">
+    <div class="header-card-green col-6">
+        <h2>Máximo y Mínimo de un conjunto de valores</h2>
+    </div>
+</div>
+<div class="row">
+    <div class="header-card-lightgreen col-6">
+        <h2>Números aleatorios: Devuelve un número aleatorio entero entre el límite mínimo y el límite máximo</h2>
+    </div>
+</div>
 <script src="js/script.js"></script>
 </body>
 </html>

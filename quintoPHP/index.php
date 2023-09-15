bb7c08317ab606fc2e9c9666a5726de584296c37
JuaNFRaN2031 <juanfran2000juanfran2000@gmail.com>
2023-09-15 15:26:42 +0200

    .

---
+++ quintoPHP/index.php
@@ -0,0 +1,27 @@
+<?php
+    include 'header.php';
+?>
+<!DOCTYPE html>
+<html lang="es-ES">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="sweetalert2.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.27/dist/sweetalert2.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<div class="banner">
+    <img src="img/" alt="">
+</div>
+
+</body>
+</html>
+<?php
+    include "footer.php";
+ ?>

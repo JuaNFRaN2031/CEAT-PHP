6ce23fb7f7ebfdca42007e462b11b1294f05523a
JuaNFRaN2031 <juanfran2000juanfran2000@gmail.com>
2023-11-17 07:40:47 +0100

    .

---
+++ actividades/concesionario/model/conexion.php
@@ -0,0 +1,25 @@
+<?php
+$servidor = "localhost";
+$user = "root";
+$clave = "";
+$bbdd = "banca_online";
+$port = "3306";
+$table = "cliente";
+
+function connect()
+{
+    global $servidor, $user, $clave, $bbdd, $table, $port;
+    $link = mysqli_connect($servidor . ":" . $port, $user, $clave);
+    if (mysqli_error($link)) {
+        echo "Existe un error en la conexión con la base de datos <br>";
+    } else {
+        // echo "Conexión establecida correctamente <br>";
+    }
+    if (!mysqli_select_db($link, $bbdd)) {
+        echo "Existe un error con la base de datos <br>";
+        exit();
+    } else {
+        // echo "Conexión con la BBDD correcta. <br>";
+    }
+    return $link;
+}
